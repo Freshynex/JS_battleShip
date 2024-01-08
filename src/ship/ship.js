77d941390279cca4916e38bc9f9d20c1c40200c5
Freshynex <bence.recsnik@gmail.com>
2024-01-08 10:26:53 +0100

    Ship object and tests done

---
+++ src/ship/ship.js
@@ -0,0 +1,20 @@
+const createShip = function(length) {
+    if (isNaN(length) || Number(length) <= 0) throw new Error('Invalid length')
+    const shipLength = length;
+    let hp = length;
+    let isSunk = false;
+
+    const takeDamage = () => {
+        if (hp === 0) return
+        hp -= 1
+        if (hp === 0) isSunk = true
+    }
+
+    const getHp = () => { return hp }
+
+    const getSunkStatus = () => { return isSunk }
+
+    return { takeDamage, getHp, getSunkStatus }
+}
+
+module.exports = createShip
---
+++ test/ship.test.js
@@ -0,0 +1,40 @@
+const createShip = require('../src/ship/ship.js')
+
+describe("Ship object", () => {
+
+	it("only accepts positive numbers > 0 as length", () => {
+		expect( () => {createShip(0)}).toThrow();
+		expect( () => {createShip(-1)}).toThrow();
+		expect( () => {createShip("asd")}).toThrow();
+	})
+
+  it("can lose health", () => {
+		let ship = createShip(2);
+		// full health
+		expect(ship.getHp()).toEqual(2);
+    ship.takeDamage();
+		// 1 damage
+		expect(ship.getHp()).toEqual(1);
+  });
+
+	it("doesnt sink when hp is above 0", () => {
+		let ship = createShip(2);
+		ship.takeDamage();
+		expect(ship.getSunkStatus()).toBe(false);
+	})
+
+	it("sinks when hp reaches 0", () => {
+		let ship = createShip(2);
+		ship.takeDamage();
+		ship.takeDamage();
+		expect(ship.getSunkStatus()).toBe(true);
+	})
+
+	it("health does not go below 0", () => {
+		let ship = createShip(1);
+		ship.takeDamage();
+		ship.takeDamage();
+		expect(ship.getHp()).toEqual(0);
+	})
+
+});
